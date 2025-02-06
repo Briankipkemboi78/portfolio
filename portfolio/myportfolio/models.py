fcd3204616c08475fa5f8dca6236321f9e88ad62
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-02-06 16:05:54 +0300

    Adding superuser

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+
+# About Model
 class About(models.Model):
     short_description = models.TextField()
     description = models.TextField()
     image = models.ImageField(upload_to="about")
 
-
     class Meta:
-        verbose_name = "About Me"
-        verbose_name_plural = "About Me"
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
 
     def __str__(self):
-        return "About Me"
-    
+        return "About me"
+
+
 # Service Model
 class Service(models.Model):
     name = models.CharField(max_length=100, verbose_name="Service name")
@@ -20,12 +22,22 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
-    
-# Recent work models
+
+
+# Recent Work Model
 class RecentWork(models.Model):
     title = models.CharField(max_length=100, verbose_name="Work title")
     image = models.ImageField(upload_to="works")
 
     def __str__(self):
         return self.title
-    
+
+
+# Client model
+class Client(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Client name")
+    description = models.TextField(verbose_name="Client say")
+    image = models.ImageField(upload_to="clients", default="default.png")
+
+    def __str__(self):
+        return self.name
