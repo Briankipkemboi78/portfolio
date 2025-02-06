ae22cf67f7a5d708eff59a8ddcf38ed33b544308
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-02-06 12:10:42 +0300

    Updating static file

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -1,50 +1,152 @@
+$(document).ready(function () {
+    'use strict';
 
-//GET SEARCH FORM AND PAGE LINKS
-let searchForm = document.getElementById('searchForm')
-let pageLinks = document.getElementsByClassName('page-link')
+    //********* page loader js
 
-//ENSURE SEARCH FORM EXISTS
-if (searchForm) {
-    for (let i = 0; pageLinks.length > i; i++) {
-        pageLinks[i].addEventListener('click', function (e) {
-            e.preventDefault()
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
 
-            //GET THE DATA ATTRIBUTE
-            let page = this.dataset.page
+    /*START MENU JS*/
+    $(window).on('scroll', function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
-            //ADD HIDDEN SEARCH INPUT TO FORM
-            searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+    //********** menu hides after click (mobile menu)
+
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+            $(this).collapse('hide');
+        }
+    });
 
 
-            //SUBMIT FORM
-            searchForm.submit()
-        })
-    }
-}
+    //*********** scrollspy js
+
+    $('body').scrollspy({
+        target: '.navbar-collapse',
+        offset: 195
+    });
+
+
+    //************ smooth scroll js
+
+    $('a.smooth-menu,a.my-btn-1,a.my-btn-2,a.my-btn-3').on("click", function (e) {
+        e.preventDefault();
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1000);
+    });
+
+    /*$('a.page-scroll').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 10
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });*/
+
+    /* Closes the Responsive Menu on Menu Item Click*/
+    $('.navbar-collapse ul li a').on('click', function() {
+        $('.navbar-toggle:visible').click();
+    });
+    /*END MENU JS*/
+
+    // Filter items when filter link is clicked
+    $(window).load(function(){
+        /*$filter.find('a').click(function () {
+            var selector = $(this).attr('data-filter');
+            $filter.find('a').removeClass('current');
+            $(this).addClass('current');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    animationDuration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+            return false;
+        });*/
+
+    });
 
 
 
-let tags = document.getElementsByClassName('project-tag')
+    //****** magnific popup
 
-for (let i = 0; tags.length > i; i++) {
-    tags[i].addEventListener('click', (e) => {
-        let tagId = e.target.dataset.tag
-        let projectId = e.target.dataset.project
+    $('.view').magnificPopup({
+        type:'image',
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+    });
 
-        // console.log('TAG ID:', tagId)
-        // console.log('PROJECT ID:', projectId)
+    $('.video-view').magnificPopup({
+        type:'iframe',
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
 
-        fetch('http://127.0.0.1:8000/api/remove-tag/', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
+
+
+
+    /*START PORTFOLIO JS*/
+
+    /*START WOW ANIMATIONS JS*/
+    new WOW().init();
+    /*END WOW ANIMATIONS JS*/
+
+
+
+    //scrollspy
+
+
+   /* $('body').scrollspy({
+        target: '.navbar-collapse',
+        offset: 195
+    });*/
+
+    //************* Owl Carousel
+
+    $(".testimonial-slider").owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        dots:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
             },
-            body: JSON.stringify({ 'project': projectId, 'tag': tagId })
-        })
-            .then(response => response.json())
-            .then(data => {
-                e.target.remove()
-            })
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
 
-    })
-}
+
+
+
+
+
+
+});
